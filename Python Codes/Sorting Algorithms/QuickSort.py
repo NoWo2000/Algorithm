b3cbba058ba3b28254efd2a20e48c7e9642e6ad4
Noah Wollenhaupt <wollenhaupt.noah@gmail.com>
2020-06-11 19:15:10 +0200

    Edited sortingCodes

---
+++ Python Codes/Sorting Algorithms/QuickSort.py
@@ -0,0 +1,10 @@
+def sort(L):
+    if L == []:
+        return L
+    x, R = L[0], L[1:]
+    S = [y for y in R if y <= x]
+    B = [y for y in R if y > x]
+
+    return sort(S) + [x] + sort(B)
+
+print(sort([9, 7, 8, 0, 5, 6, 4, 1, 2, 3]))
